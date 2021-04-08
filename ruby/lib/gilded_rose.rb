ee9a9d6b3b676cf7d04dd5597e6c18d9074d94c4
Eds <steadyede@live.co.uk>
2021-04-08 19:06:12 +0100

    refactored gilded rose for clarity

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -4,12 +4,10 @@ class GildedRose
     @items = items
   end
 
-
-
-
   def update_quality()
     @items.each do |item|
       next if item.name == "Sulfuras, Hand of Ragnaros"
+      item.sell_in = item.sell_in - 1
 
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
@@ -19,20 +17,15 @@ class GildedRose
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
+            if item.sell_in < 11 && item.quality < 50
                 item.quality = item.quality + 1
-              end
             end
-            if item.sell_in < 6
-              if item.quality < 50
+            if item.sell_in < 6 && item.quality < 50
                 item.quality = item.quality + 1
-              end
             end
           end
         end
       end
-        item.sell_in = item.sell_in - 1
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
