4409ee445baf816bdb9f53dec7d758c78d0083b3
Eds <steadyede@live.co.uk>
2021-04-08 22:59:22 +0100

    aged brie shortened

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -31,17 +31,17 @@ class GildedRose
       item.sell_in = item.sell_in - 1
 
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-                item.quality = item.quality - 1
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+        if item.name == "Aged Brie" && item.quality < 50
+          item.quality = item.quality + 1
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
+          if item.name != "Aged Brie"
+            if item.name != "Backstage passes to a TAFKAL80ETC concert"
+              if item.quality > 0
+                  item.quality = item.quality - 1
+              end
+            else
+              item.quality = item.quality - item.quality
+            end
           end
         end
       end
