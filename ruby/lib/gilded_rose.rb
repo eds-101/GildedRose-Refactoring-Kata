9c8e518f3de23fd5265df7030b92ec98d094f90b
Eds <steadyede@live.co.uk>
2021-04-08 22:40:47 +0100

    refactored backstage for removing if statement

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -8,21 +8,20 @@ class GildedRose
     @items.each do |item|
       next if item.name == "Sulfuras, Hand of Ragnaros"
 
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality < 50
+      if item.name == "Backstage passes to a TAFKAL80ETC concert" && item.quality < 50
+        item.quality = item.quality + 1
+        if item.sell_in < 11 && item.quality < 50
           item.quality = item.quality + 1
-            if item.sell_in < 11 && item.quality < 50
-              item.quality = item.quality + 1
-            end
-            if item.sell_in < 6 && item.quality < 50
-              item.quality = item.quality + 1
-            end
+        end
+        if item.sell_in < 6 && item.quality < 50
+          item.quality = item.quality + 1
         end
       end
 
       if item.name == "Aged Brie" && item.quality < 50
         item.quality = item.quality + 1
       end
+
       if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert")
         if item.quality > 0
             item.quality = item.quality - 1
