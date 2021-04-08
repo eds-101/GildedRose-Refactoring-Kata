221a767a088bd8ca9a59c0d88d826943e8c7018a
Eds <steadyede@live.co.uk>
2021-04-08 16:58:29 +0100

    refactoring sulfuras out

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -4,13 +4,18 @@ class GildedRose
     @items = items
   end
 
+
+
+
   def update_quality()
     @items.each do |item|
+      next if item.name == "Sulfuras, Hand of Ragnaros"
+
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
+          # if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
-          end
+          # end
         end
       else
         if item.quality < 50
@@ -29,16 +34,16 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      # if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
-      end
+      # end
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+              # if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
-              end
+              # end
             end
           else
             item.quality = item.quality - item.quality
