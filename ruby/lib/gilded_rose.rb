a73a1d5889b16d4419ba322d68f3c89dae51e9a8
Eds <steadyede@live.co.uk>
2021-04-08 16:59:55 +0100

    refactored sulfuras

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -13,9 +13,7 @@ class GildedRose
 
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          # if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
-          # end
         end
       else
         if item.quality < 50
@@ -34,16 +32,12 @@ class GildedRose
           end
         end
       end
-      # if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
-      # end
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              # if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
-              # end
             end
           else
             item.quality = item.quality - item.quality
