7fb8b756f6f6a6ce85904a04ad12e11350ac04e9
Eds <steadyede@live.co.uk>
2021-04-08 18:44:52 +0100

    tests for backstage passes

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -11,7 +11,7 @@ describe GildedRose do
 
     describe "Brie" do
 
-      it "increases in quality each day" do
+      describe "increases in quality each day" do
         context "+1 whilst sell in is positive" do
 
         end
@@ -25,17 +25,34 @@ describe GildedRose do
 
     end
 
+
     describe "Backstage passes" do
       it "increases in quality each day" do
-        context "+2 when sell in is 10-6 days out" do
-          
-        end
-        context "+3 when sell in is 5-0 days out" do
-          
-        end
+        @items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 13, 5)]
+        GildedRose.new(@items).update_quality()
+        expect(@items[0].quality).to eq 6
+        GildedRose.new(@items).update_quality()
+        expect(@items[0].quality).to eq 7
       end
-      it "loses all quality once concert passes" do
-      
+      describe "when concert date is close" do
+        it "increases quality by 2 for 10-6 days out" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 7, 8)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 10
+        end
+        it "increases quality by 3 for 5-0 days out" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 4, 10)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 13
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 16
+        end
+        it "loses all quality once concert passes" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 100)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 0
+        end
+
       end
 
     end
@@ -52,6 +69,7 @@ describe GildedRose do
         expect(@items[0].sell_in).to eq 5
       end
     end
+
 
   end
 
